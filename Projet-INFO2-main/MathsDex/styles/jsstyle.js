034fe24ef7963e2cfb89ad2f3212cda35a34b613
Celian28 <celian.pithon@gmail.com>
2020-12-14 11:17:43 +0100

    Modif profil.html et javascript

--- Projet-INFO2-main/MathsDex/styles/jsstyle.js
+++ Projet-INFO2-main/MathsDex/styles/jsstyle.js
@@ -5,3 +5,15 @@ $( "#b1" ).click(function() {
 $( "#b2" ).click(function() {
   $( ".c2" ).slideToggle( "slow" );
 });
+
+$( "#b3" ).click(function() {
+  $( ".c3" ).slideToggle( "slow" );
+});
+
+$( "#b4" ).click(function() {
+  $( ".c4" ).slideToggle( "slow" );
+});
+
+$( "#b5" ).click(function() {
+  $( ".c5" ).slideToggle( "slow" );
+});
